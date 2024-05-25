ca8afda54a12dc282c43cff364b936b55b3d2aec
yhanz15 <yhanz.kd15@gmail.com>
2024-05-26 07:13:34 +0800

    updates

---
+++ src/js/team.js
@@ -0,0 +1,61 @@
+import photoYhanz from "../images/team/yhanz.jpg";
+import photoApril from "../images/team/april.jpg";
+import photoJulius from "../images/team/julius.jpg"; 
+import photoMartin from "../images/team/martin.jpg";  
+import photoJansen from "../images/team/jansen.jpg"; 
+import photoMJ from "../images/team/mj.jpg";
+import photoRomulo from "../images/team/romulo.jpg"; 
+import photoJon from "../images/team/jon.jpg";
+
+
+export const teamList = [
+  {
+    name: 'Ian To-ong',
+    role: 'Team Lead',
+    github: 'https://github.com/yhanz15',
+    photo: photoYhanz,
+  },
+  {
+    name: 'April Espiritu',
+    role: 'Scrum Master',
+    github: 'https://github.com/aprilespiritu',
+    photo: photoApril,
+  },
+  {
+    name: 'Julius Baraquiel',
+    role: 'Developer',
+    github: 'https://github.com/JuliusB-ui',
+    photo: photoJulius,
+  },
+  {
+    name: 'Martin Dato',
+    role: 'Developer',
+    github: 'https://github.com/king-dato',
+    photo: 'photoMartin',
+  },
+  {
+    name: 'Jansen Ray Jemio',
+    role: 'Developer',
+    github: 'https://github.com/lukamatcha12',
+    photo: 'photoJansen',
+  },
+  {
+    name: 'Mary Jane Magamba',
+    role: 'Developer',
+    github: 'https://github.com/marichiiin',
+    photo: photoMJ,
+  },
+  {
+    name: 'Romulo Pahuyo',
+    role: 'Developer',
+    github: 'https://github.com/romulopahuy',
+    photo: photoRomulo,
+  },
+  {
+    name: 'Jon Salazar',
+    role: 'Developer',
+    github: 'https://github.com/jonsalazar09',
+    photo: photoJon,
+  },
+
+];
